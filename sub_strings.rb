98de32f6c03e54b6bbdda9bff16a382b1eb223ac
TedLivist <livistteddy@gmail.com>
2020-11-24 15:33:00 +0100

    contains solution

---
+++ sub_strings.rb
@@ -0,0 +1,19 @@
+def substrings(word, dictionary)
+  word_arr = word.split(/[,!.?;:'" "]/)
+
+  low_case = word_arr.each do |str|
+    str.downcase!
+  end
+
+  result = Hash.new(0)
+
+  for sub in low_case
+    for text in dictionary
+      if text == sub or sub.include? text
+        result[text] += 1
+      end
+    end
+  end
+
+  result
+end
